15b371c8c2cf3b8e014ac5a3d6300ed4d3cdfc78
Qin-MacBookPro <qinyupeng1>
2023-07-09 13:17:06 -0700

    update

--- Assets/Scripts/CubeWorld/CubeManagerSystem.cs
+++ Assets/Scripts/CubeWorld/CubeManagerSystem.cs
@@ -3,7 +3,7 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
-
+[UpdateAfter(typeof(CubeRespawnSystem))]
 public partial struct CubeManagerSystem : ISystem
 {
     [BurstCompile]
@@ -15,30 +15,50 @@ public partial struct CubeManagerSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         float deltaTime = SystemAPI.Time.DeltaTime;
-        //foreach complexity O(n)
+
+
+        //**use command buffer to add component, if no CubeTag, add it, add class type data
+        var ecb = new EntityCommandBuffer(Allocator.Temp);
+        foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithNone<CubeTag>().WithEntityAccess())
+        {
+            ecb.AddComponent(entity, new CubeTag { tag = true });
+            ecb.AddComponent(entity, new ClassCubeData()); //this is not working, because it is not in the same world as state.EntityManager
+        }
+        //!disable Entity with CubeTag and tag is false
+        foreach (var (cubeTag, entity) in SystemAPI.Query<RefRO<CubeTag>>().WithEntityAccess())
+        {
+            if (!cubeTag.ValueRO.tag)
+                ecb.AddComponent<Disabled>(entity);
+        }
+        //!Enable Entity with disabled component
+        foreach (var (cubeTag, entity) in SystemAPI.Query<RefRO<CubeTag>>().WithAll<Disabled>().WithEntityAccess())
+        {
+            if (cubeTag.ValueRO.tag)
+                ecb.RemoveComponent<Disabled>(entity);
+        }
+        ecb.Playback(state.EntityManager);
+
+
+        //**foreach complexity O(n)
         // foreach (var (transform, cubeData) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<CubeData>>())
         // {
         //     transform.ValueRW = transform.ValueRO.RotateY(cubeData.ValueRO.speed * deltaTime);
         // }
 
-        // job complexity O(logN)
+        //**job complexity O(logN)
         var job = new CubeJob { deltaTime = deltaTime };
-        job.Schedule();
+        job.ScheduleParallel();
 
-        //foreach complexity O(n) with IAspect
-        // foreach (var cubeIAspect in SystemAPI.Query<CubeIAspect>())
+        // **foreach complexity O(n) with IAspect
+        // foreach (var (cubeIAspect,entity) in SystemAPI.Query<CubeIAspect>().WithEntityAccess())
         // {
         //     cubeIAspect.Rotate(deltaTime);
         // }
 
-        //use command buffer to add component, if no CubeTag, add it, add class type data
-        // var ecb = new EntityCommandBuffer(Allocator.Temp);
-        // foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithNone<CubeTag>().WithEntityAccess())
-        // {
-        //     ecb.AddComponent(entity, new CubeTag { tag = true });
-        //     ecb.AddComponent(entity, new ClassCubeData()); //this is not working, because it is not in the same world as state.EntityManager
-        // }
-        // ecb.Playback(state.EntityManager);
+
+
+
+
     }
 }
 [BurstCompile]
--- Assets/Scripts/CubeWorld/CubeRespawn/CubeRespawnSystem.cs
+++ Assets/Scripts/CubeWorld/CubeRespawn/CubeRespawnSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+
 public partial struct CubeRespawnSystem : ISystem
 {
     uint updateCounter;
@@ -16,7 +17,11 @@ public partial struct CubeRespawnSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //find if empty, if empty, add new cubes
+        //**run once when code start
+        state.Enabled = false;
+
+
+        //**find if empty, if empty, add new cubes
         // var spinningCubesQuery = SystemAPI.QueryBuilder().WithAll<CubeData>().Build();
         // if (spinningCubesQuery.IsEmpty)
         // {
@@ -30,16 +35,36 @@ public partial struct CubeRespawnSystem : ISystem
         //     }
         // }
 
-        //run once when code start
-        state.Enabled = false;
+
+
+        //**instance new cubes with specific number
         var prefab = SystemAPI.GetSingleton<CubeRespawn>().prefab;
-        var instance = state.EntityManager.Instantiate(prefab, 500000, Allocator.Temp);
         var random = Unity.Mathematics.Random.CreateFromIndex(updateCounter++);
+        var instance = state.EntityManager.Instantiate(prefab, 100, Allocator.Temp);
         foreach (var entity in instance)
         {
             var transform = SystemAPI.GetComponentRW<LocalTransform>(entity);
             transform.ValueRW.Position = random.NextFloat3(new float3(-100, -100, -100), new float3(100, 100, 100));
         }
+
+        //**chunk query
+        // EntityQuery query = SystemAPI.QueryBuilder().WithAll<LocalTransform, CubeTag>().Build();
+        // NativeArray<ArchetypeChunk> chunks = query.ToArchetypeChunkArray(Allocator.Temp);
+        // ComponentTypeHandle<LocalTransform> localTransformTypeHandle = SystemAPI.GetComponentTypeHandle<LocalTransform>(true);
+        // foreach (ArchetypeChunk chunk in chunks)
+        // {
+        //     NativeArray<LocalTransform> localTransforms = chunk.GetNativeArray(ref localTransformTypeHandle);
+        //     for (int i = chunk.Count; i < chunk.Count; i++)
+        //     {
+        //         LocalTransform obstacleTransform = localTransforms[i];
+        //         Entity player = state.EntityManager.Instantiate(prefab);
+        //         state.EntityManager.SetComponentData(player, new LocalTransform
+        //         {
+        //             Position = random.NextFloat3(new float3(-100, -100, -100), new float3(100, 100, 100))
+        //         });
+
+        //     }
+        // }
     }
 }
 
