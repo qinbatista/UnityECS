ebe96ba7d61da9cb7d22df70c1fd767411e05b50
Qin-MacBookPro <qinyupeng1>
2023-07-09 13:36:25 -0700

    aaa

--- Assets/Scripts/CubeWorld/CubeManagerSystem.cs
+++ Assets/Scripts/CubeWorld/CubeManagerSystem.cs
@@ -12,6 +12,7 @@ public partial struct CubeManagerSystem : ISystem
         state.RequireForUpdate<CubeData>();
     }
 
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
